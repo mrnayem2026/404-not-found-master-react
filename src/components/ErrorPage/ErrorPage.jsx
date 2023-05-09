a164b845923e16b90324b8645264a0e9fc5c6b05
Mostafizur Rahman nayem <hpbd17062022@gmail.com>
2023-05-09 07:03:53 +0600

    All done

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,11 @@
 const ErrorPage = () => {
     return (
-        <div>
+        <div className="container mx-auto p-4 ">
             {/* Logo start  */}
-            <h1 className="">404 not found</h1>
+            <h1 className="lg:py-10 font-Inconsolata text-2xl font-bold uppercase">404 not found</h1>
 
             {/* main content start  */}
-            <div>
+            <div className="flex flex-col md:flex-row lg:gap-64 pt-16 lg:py-60">
                 {/* left side start  */}
                 <div> 
                     <img src="/src/assets/scarecrow.png" alt="scarecorow" className="h-96" />
@@ -13,13 +13,13 @@ const ErrorPage = () => {
                 {/* left side end */}
 
                 {/* Right side start  */}
-                <div>
-                    <h1>I have bad news for you</h1>
-                    <p>The page you are looking
-                        for might be removed or is 
-                        temporarily unavailable</p>\
+                <div className="pt-7">
+                    <h1 className="font-Space_Mono font-bold text-6xl">I have bad news <br /> for you</h1>
+                    <p className="lg:py-9 text-xl font-Space_Mono font-normal text-gray-500 py-8">The page you are looking <br />
+                        for might be removed or is <br />
+                        temporarily unavailable</p>
 
-                    <button>Back to homepage</button>
+                    <button className="py-6 px-11 bg-black text-white text-sm font-Space_Mono font-medium uppercase">Back to homepage</button>
                 </div>
                 {/* Right side end */}
             </div>
