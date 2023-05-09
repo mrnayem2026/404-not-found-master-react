7de1b7bdcaa647ee2e266dd1c0b924223d74f02f
Mostafizur Rahman nayem <hpbd17062022@gmail.com>
2023-05-09 06:17:44 +0600

    404-not-found-master-react primary setup  done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import ErrorPage from './components/ErrorPage/ErrorPage'
+
+function App() {
+
+
+  return (
+    <>
+      <ErrorPage></ErrorPage>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,9 @@
+const ErrorPage = () => {
+    return (
+        <div>
+            I  am From Erro Page.
+        </div>
+    );
+};
+
+export default ErrorPage;
